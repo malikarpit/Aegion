a2a03be0ec05cb4f32bfb7bb26487547924a28a7
Arpit <malikarpit40@gmail.com>
2026-02-28 20:18:00 +0530

    feat: scaffold VS Code extension with basic commands

---
+++ aegion-vscode/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    target: 'node',
+    mode: 'none',
+    entry: './src/extension.ts',
+    output: {
+        path: path.resolve(__dirname, 'out'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2'
+    },
+    externals: {
+        vscode: 'commonjs vscode'
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+        extensionAlias: {
+            '.js': ['.ts', '.js']
+        },
+        alias: {
+            bufferutil: false,
+            'utf-8-validate': false
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    devtool: 'nosources-source-map'
+};
